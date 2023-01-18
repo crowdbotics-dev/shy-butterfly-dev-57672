c7503fd1627c1459a88854be6f2e8259160ee15e
Crowdbotics <engineering@crowdbotics.com>
2023-01-18 05:41:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-butterfly-dev-57672",
+  url: "https://shy-butterfly-dev-57672.botics.co",
+  api: "https://shy-butterfly-dev-57672.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
